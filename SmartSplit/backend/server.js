c6864a9b5f56eb2f2a21cd51d84d06fae27e23c9
JillianK1219 <tur02823@temple.edu>
2026-03-26 14:59:56 -0400

    resolve localhost access issue by binding server to 127.0.0.1

--- SmartSplit/backend/server.js
+++ SmartSplit/backend/server.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
   res.send("SmartSplit backend running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+app.listen(5000, "127.0.0.1", () => {
+  console.log("Server running on http://127.0.0.1:5000");
 });
