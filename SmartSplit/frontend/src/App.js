23edf5cc174bebbcdb4d0af10f7a2f87bf194888
Akil <akiltalakola@gmail.com>
2026-04-01 20:05:36 -0400

    Added homepage with create and join household buttons

--- SmartSplit/frontend/src/App.js
+++ SmartSplit/frontend/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>SmartSplit</h1>
+
+        <p>Select an option to continue:</p>
+
+        <button onClick={() => alert("Create Household clicked")}>
+          Create Household
+        </button>
+
+        <button onClick={() => alert("Join Household clicked")}>
+          Join Household
+        </button>
       </header>
     </div>
   );
