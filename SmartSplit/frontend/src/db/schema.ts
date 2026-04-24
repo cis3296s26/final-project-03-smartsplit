02bd6ace2d16e2b72afce51bf475f57e2560194b
JillianK1219 <tur02823@temple.edu>
2026-04-24 12:00:51 -0400

    updating schema to match server type

--- SmartSplit/frontend/src/db/schema.ts
+++ SmartSplit/frontend/src/db/schema.ts
@@ -8,3 +8,33 @@ export const Household = mysqlTable('Household', {
   joinKey: varchar({ length: 36 }).unique().notNull(),
   created_at: timestamp('created_at').defaultNow(),
 });
+
+
+export const User = mysqlTable('User', {
+  userId: varchar('userId', { length: 36 }).primaryKey().notNull(),
+  name: varchar('name', { length: 100 }).notNull(),
+  email: varchar('email', { length: 100 }).notNull().unique(),
+});
+
+
+export const HouseholdMember = mysqlTable('HouseholdMember', {
+  userId: varchar('userId', { length: 36 }).notNull().references(() => User.userId),
+  householdId: varchar('householdId', { length: 36 }).notNull().references(() => Household.householdId),
+});
+
+export const Expense = mysqlTable('Expense', {
+  expenseId: varchar('expenseId', { length: 36 }).primaryKey().notNull(),
+  description: varchar('description', { length: 255 }),
+  amount: decimal('amount', { precision: 10, scale: 2 }).notNull(),
+  expenseDate: date('expenseDate').notNull(),
+  splitType: mysqlEnum('splitType', ['Equal', 'Percentage', 'Custom']).notNull(),
+  category: mysqlEnum('category', ['Rent', 'Utilities', 'Groceries', 'Subscriptions', 'Other']).notNull(),
+  householdId: varchar('householdId', { length: 36 }).references(() => Household.householdId),
+});
+
+export const ExpenseShare = mysqlTable('ExpenseShare', {
+  shareId: varchar('shareId', { length: 36 }).primaryKey().notNull(),
+  amountOwed: decimal('amountOwed', { precision: 10, scale: 2 }).notNull(),
+  userId: varchar('userId', { length: 36 }).references(() => User.userId),
+  expenseId: varchar('expenseId', { length: 36 }).references(() => Expense.expenseId),
+});
